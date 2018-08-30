18dd8f11799ffaf982dc802903ddd15568514dbd
Shlomi Atzmon <shlomiatzmon82u@gmail.com>
2018-08-31 00:38:10 +0300

    adding EntrustUserTrait to user model

--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable, EntrustUserTrait;
 
     protected $fillable = ['name', 'email', 'password'];
     protected $hidden = ['password', 'remember_token'];
